767fd424188271c14936bfbe002fe81f2dc1cab8
Anand Nishad <112419322+anand129-dev@users.noreply.github.com>
2024-08-23 22:36:25 +0530

    Updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
-import ReactMarkdown from "react-markdown";
-import { useState } from "react";
-import "./App.css";
-import axios from "axios";
+import Main from "./components/main/Main"
+import Sidebar from "./components/sidebar/Sidebar"
 
-function App() {
-  const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState("");
-
-  //Make the function asynchronous
-  async function generateAnswer() {
-    setAnswer("loading...");
-    // console.log("loading...");
-    const response = await axios({
-      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCrJSl1WFWgGNCIl5vVEZQznTW4N0hHNU4",
-      //Use ENP to encrypt the API key
-      method: "post",
-      data: {
-        contents: [{ parts: [{ text: question }] }],
-      },
-    });
-
-    setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"]);
-    
-    // console.log(
-    //   setAnswer(response["data"]["candidates"][0]["content"]["parts"][0]["text"])
-    // );
-  }
-
-  //Json Combination of Arrays & Objects in Javascript.
-
+const App = () => {
   return (
-    <>
-      <h1>AI-ChatApp</h1>
-      <textarea
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        cols="30"
-        rows="10"
-      ></textarea>
-      <button onClick={generateAnswer}>Generate answer</button>
-      
-      <p>
-        <ReactMarkdown>
-          {answer}
-        </ReactMarkdown>
-      </p>
-  
+    <><Sidebar/>
+    <Main/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
